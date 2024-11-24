98610a92ec45cd6b5396e47e54ed3997c227909e
MrRique15 <henrique-favaro@hotmail.com>
2024-11-24 16:42:48 -0300

    fix: change text files dir to .env file

--- src/app/api/save/route.ts
+++ src/app/api/save/route.ts
@@ -2,8 +2,7 @@ import { NextResponse } from "next/server";
 import fs from "fs/promises";
 import path from "path";
 
-// TODO: Move this to .env
-const DATA_DIR = path.join(process.cwd(), "text_data");
+const DATA_DIR = path.join(process.cwd(), process.env.TEXT_FILES || "text_data");
 
 async function ensureDataDirectory() {
   try {
